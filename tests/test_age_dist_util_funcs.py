c898bdf1776adf34f288308b80c6a2cba6e73bba
Avi Flamholz <flamholz@gmail.com>
2025-07-11 11:23:14 -0400

    make a test fixture for age dist test

--- tests/test_age_dist_util_funcs.py
+++ tests/test_age_dist_util_funcs.py
@@ -1,38 +1,34 @@
 import numpy as np
 import pandas as pd
-from .. import age_distribution_util_funcs as util
+import pytest
+from soil_diskin import age_dist_utils as util
 
+def test_age_dist():
+    # Test the validity of the age distribution function by comparing the results of a specific model in 
+    # Sierra et al. 2018. Specifically, we look at the RothC results.
+    A = np.array([[-10, 0, 0, 0],
+                [0, -0.3, 0, 0],
+                [1.02, 0.03, -0.59, 0],
+                [1.2, 0.04, 0.08, -0.02]
+                ])
+    ksRC = np.array([10, 0.3, 0.66, 0.02])
+    FYMsplit = np.array([0.49, 0.49, 0.02])
+    DR=1.44; In=1.7; FYM=0; clay=23.4
+    x = 1.67 * (1.85 + 1.60 * np.exp(-0.0786 * clay))
+    B = 0.46 / (x + 1) # Proportion that goes to the BIO pool
+    H = 0.54 / (x + 1) # Proportion that goes to the HUM pool
 
-# Test the validity of the age distribution function by comparing the results of a specific model in 
-# Sierra et al. 2018. Specifically, we look at the RothC results.
+    ai3 = B * ksRC
+    ai4 = H * ksRC
 
+    ARC = np.diag(-ksRC)
+    ARC[2,:] = ARC[2,:] + ai3
+    ARC[3,:] = ARC[3,:] + ai4
 
-A = np.array([[-10, 0, 0, 0],
-              [0, -0.3, 0, 0],
-              [1.02, 0.03, -0.59, 0],
-              [1.2, 0.04, 0.08, -0.02]
-              ])
-ksRC = np.array([10, 0.3, 0.66, 0.02])
-FYMsplit = np.array([0.49, 0.49, 0.02])
-DR=1.44; In=1.7; FYM=0; clay=23.4
-x = 1.67 * (1.85 + 1.60 * np.exp(-0.0786 * clay))
-B = 0.46 / (x + 1) # Proportion that goes to the BIO pool
-H = 0.54 / (x + 1) # Proportion that goes to the HUM pool
+    RcI=np.array([In * (DR / (DR + 1)) + (FYM * FYMsplit[0]), In * (1 / (DR + 1)) + (FYM * FYMsplit[1]), 0, (FYM * FYMsplit[2])])
 
-ai3 = B * ksRC
-ai4 = H * ksRC
+    ages = np.arange(1,1001)
+    pA = util.box_model_ss_age_dist(ARC,RcI,ages)
 
-ARC = np.diag(-ksRC)
-ARC[2,:] = ARC[2,:] + ai3
-ARC[3,:] = ARC[3,:] + ai4
-
-RcI=np.array([In * (DR / (DR + 1)) + (FYM * FYMsplit[0]), In * (1 / (DR + 1)) + (FYM * FYMsplit[1]), 0, (FYM * FYMsplit[2])])
-
-ages = np.arange(1,1001)
-
-
-pA = util.box_model_ss_age_dist(ARC,RcI,ages)
-
-sierra = pd.read_csv('notebooks/tests/sierra_2018_RothC.csv')
-
-np.testing.assert_almost_equal(pA.flatten(), sierra['age_pdf'].values)
+    sierra = pd.read_csv('notebooks/tests/sierra_2018_RothC.csv')
+    np.testing.assert_almost_equal(pA.flatten(), sierra['age_pdf'].values)
