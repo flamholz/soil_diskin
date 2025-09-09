b6e2e5aa8197404b77ac9ae2a291d0b8d2f7f6c3
Avi Flamholz <flamholz@gmail.com>
2025-09-09 17:22:00 -0400

    allow the data files to be downloaded outside the scripts, e.g. by snakemake

--- notebooks/01_preprocess_balesdent_data_all.py
+++ notebooks/01_preprocess_balesdent_data_all.py
@@ -1,9 +1,5 @@
-#%% 00 - load libraries
 import os
-
 import pandas as pd
-import numpy as np
-from soil_diskin.utils import download_file
 from soil_diskin.data_wrangling_all import process_balesdent_data
 
 """
@@ -15,16 +11,15 @@ saved to a CSV file in the results folder.
 """
 
 if __name__ == "__main__":
-    folder = "../data/balesdant_2018/"
-    raw_data_filename = "41586_2018_328_MOESM3_ESM.xlsx"
-    raw_data_url = "https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-018-0328-3/MediaObjects/41586_2018_328_MOESM3_ESM.xlsx"
-    download_file(raw_data_url, folder, raw_data_filename)
+    # Data should be downloaded by snakemake already
+    folder = "data/balesdent_2018/"
+    raw_data_filename = "balesdent_2018_raw.xlsx"
+    raw_data_path = os.path.join(folder, raw_data_filename)
+    raw_data = pd.read_excel(raw_data_path, skiprows=7)
 
-    raw_data = pd.read_excel(os.path.join(folder,raw_data_filename),skiprows=7)
     final_data = process_balesdent_data(raw_data)
-    final_data.to_csv(os.path.join('results/processed_balesdant_2018_all.csv'), index=False)
+    final_data.to_csv(os.path.join('results/processed_balesdent_2018_all.csv'), index=False)
 
-## TODO
+## TODO merge with 01_preprocess_balesdent_data.py
 
 # 1. Test that the processing works as expected for a predefined set of data, including data with missing values in deeper layers
-# %%
