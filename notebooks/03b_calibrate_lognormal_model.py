f7b0d8ce96cdce3bdb51cd27d6341fa374775d29
Avi Flamholz <flamholz@gmail.com>
2025-09-21 21:10:30 -0400

    load the right files

--- notebooks/03b_calibrate_lognormal_model.py
+++ notebooks/03b_calibrate_lognormal_model.py
@@ -6,13 +6,16 @@ import matplotlib.pyplot as plt
 # Read the age scan data and the radiocarbon data
 # the Mathematica script must be run first to generate the age scan
 # (notebooks/03b_calibrate_lognormal_model.wls)
-site_data = pd.read_csv('results/tropical_sites_14C_turnover.csv')
+site_data = pd.read_csv('results/all_sites_14C_turnover.csv')
 age_scan = pd.read_csv('results/03_calibrate_models/03b_lognormal_model_age_scan.csv', header=None)
+# TODO: save the ages in the age scan file? 
 ages = 10**np.arange(3,5.5,(5.5-3)/100)
 ages = np.concatenate([ages, np.array([10**5.5])])
 
-predictions = pd.read_csv('results/03b_lognormal_site_parameters.csv', header=None)
-# predict the mean age for each site based on the calibration curve
+predictions = pd.read_csv('results/03b_lognormal_site_parameters.csv',
+                          header=None)
+# predict the mean age for each site by interpolating the age scan 
+# calibration curve generated in Mathematica
 preds = []
 for site in site_data.index:
     calcurve = interp1d(age_scan.iloc[site], ages)
