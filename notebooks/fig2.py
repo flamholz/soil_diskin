fa41e23c716b96f60bdbf1ceebd15991ba2c001c
Avi Flamholz <flamholz@gmail.com>
2025-11-13 17:19:09 -0500

    code for generating figure 2 separately

--- notebooks/fig2.py
+++ notebooks/fig2.py
@@ -1,74 +1,142 @@
-import pickle
-import pandas as pd
+# %%
 import numpy as np
-from scipy.integrate import cumulative_trapezoid
-from scipy.interpolate import interp1d
-from scipy.optimize import minimize
-from sklearn.metrics import r2_score, root_mean_squared_error
-from scipy.stats import pearsonr, spearmanr
-from notebooks.models import PowerLawDisKin
-from notebooks.constants import INTERP_R_14C, C14_DATA
 import matplotlib.pyplot as plt
-from permetrics.regression import RegressionMetric
 
-#%% Load the site data
-tropical_sites = pd.read_csv('results/processed_balesdent_2018.csv')
+from models import PowerLawDisKin
 
-#%% Load the predictions
-# powerlaw_predictions = pd.read_csv('results/04_model_predictions/power_law_16-07-2025.csv',header=None, names=['prediction'])
-powerlaw_predictions = pd.read_csv(f'results/04_model_predictions/power_law.csv',header=None, names=['prediction'])
-lognormal_predictions = pd.read_csv(f'results/04_model_predictions/lognormal.csv',header=None, names=['prediction'])
-gamma_predictions = pd.read_csv(f'results/04_model_predictions/gamma.csv',header=None, names=['prediction'])    
-CLM45_predictions = pd.read_csv(f'results/04_model_predictions/CLM45_fnew.csv', header=None, names=['prediction'])
-JSBACH_predictions = pd.read_csv(f'results/04_model_predictions/JSBACH_fnew.csv', header=None, names=['prediction'])
-RCM_predictions = pd.read_csv(f'results/04_model_predictions/RCM.csv')
-# %% Define function to plot model predictions
-def plot_model_predictions(ax, predictions, model_name, color):
-    ax.plot([0, 1], [0, 1], color='k', linestyle='-', label='y=x')
-    ax.scatter(tropical_sites['total_fnew'], predictions, label=model_name, color=color)
+import viz
+colors = viz.color_palette()
 
-    evaluator = RegressionMetric(y_true=tropical_sites['total_fnew'].values, y_pred=predictions.values)
-    rmse = root_mean_squared_error(tropical_sites['total_fnew'], predictions)
-    ax.text(0.05, 0.95, f'KGE: {evaluator.kling_gupta_efficiency():.3f}', transform=ax.transAxes, fontsize=10, verticalalignment='top')
-    ax.text(0.05, 0.90, f'RMSE: {rmse:.3f}', transform=ax.transAxes, fontsize=10, verticalalignment='top')
-    ax.set(xlabel='observed', ylabel='predicted')
-    ax.set_title(model_name) 
+# use the style file
+plt.style.use('notebooks/style.mpl')
 
-# %% Plot the predictions predictions
-colors = plt.cm.tab10.colors
+# %% Load the results of the t_min, t_max scan
+data = np.load('results/fig2_calcs.npz')
+t_min_values = data['t_min_values']
+t_max_values = data['t_max_values']
+T_matrix = data['T_matrix']
+R_14C_matrix = data['R_14C_matrix']
 
+t_max_grid, t_min_grid = np.meshgrid(
+    t_max_values, t_min_values, indexing='ij')
 
-fig, axs = plt.subplots(2,4,figsize=(15, 8), dpi=600,constrained_layout=True)
-axs = axs.flatten()
+# %% For a few target T and R_14C values, find the model
+# parameters that best reproduce those values.
 
-# power-law model predictions
-plot_model_predictions(axs[0], powerlaw_predictions['prediction'], 'Power Law Model', colors[0])
+target_Ts = [1, 3, 10, 50]
+target_R_14Cs = [1.1, 1., 0.88, 0.78]
+calibrated_Ts = []
+calibrated_R_14Cs = []
+calibrated_models = []
 
-# lognormal model predictions
-plot_model_predictions(axs[1], lognormal_predictions['prediction'], 'Lognormal Model', colors[1])
+for target_T, target_R_14C in zip(target_Ts, target_R_14Cs):
+    # find positions where T = target_T and R_14C = target_R_14C
+    T_positions = np.argwhere(np.isclose(T_matrix, target_T, rtol=0.1))
+    R_14C_positions = np.argwhere(np.isclose(R_14C_matrix, target_R_14C, rtol=0.1))
 
-# gamma model predictions
-plot_model_predictions(axs[2], gamma_predictions['prediction'], 'Gamma Model', colors[2])
+    pct_diffs_T = 100 * (T_matrix - target_T) / target_T
+    pct_diffs_R_14C = 100 * (R_14C_matrix - target_R_14C) / target_R_14C
+    loss_mat = (pct_diffs_T**2 + pct_diffs_R_14C**2)
+    min_idx = np.unravel_index(np.argmin(loss_mat), loss_mat.shape)
 
-# CLM4.5 predictions
-plot_model_predictions(axs[3], CLM45_predictions['prediction'], 'CLM4.5 Model', colors[3])
+    print(f'For target T={target_T}, R_14C={target_R_14C}:')
+    print(f'Calibrated parameters: t_min = {t_min_values[min_idx[0]]}, t_max = {t_max_values[min_idx[1]]}')
+    print(f'With Turnover Time = {T_matrix[min_idx]}, R_14C = {R_14C_matrix[min_idx]}')
+    print(f'Percent differences: dT/T = {pct_diffs_T[min_idx]}, dR_14C/R_14C = {pct_diffs_R_14C[min_idx]}')
+    print('')
 
-# JSBACH predictions
-plot_model_predictions(axs[4], JSBACH_predictions['prediction'], 'JSBACH Model', colors[4])
+    calibrated_Ts.append(T_matrix[min_idx])
+    calibrated_R_14Cs.append(R_14C_matrix[min_idx])
+    calibrated_model = PowerLawDisKin(
+        t_min=t_min_values[min_idx[0]],
+        t_max=t_max_values[min_idx[1]])
+    calibrated_models.append(calibrated_model)
 
-# Reduced complexity model predictions
-for i, col in enumerate(RCM_predictions.columns):
-    plot_model_predictions(axs[5 + i], RCM_predictions[col], col +  ' (corrected for $^{14} C$)', colors[5 + i])
+# %% Plot figure 2, which diagrams the calibration procedure
+mosaic = 'ABC\nDEF'
+fig, axs = plt.subplot_mosaic(
+    mosaic, layout='constrained', figsize=(7.25, 4))
 
-# # remove empty subplots
-# for i in range(4 + len(RCM_predictions.columns), 8):
-#     fig.delaxes(axs[i])
+# Panel A -- contour plot of turnover time with log-scaled axes
+plt.sca(axs['B'])
+plt.contourf(t_max_grid, t_min_grid, T_matrix,
+             levels=10, cmap='Blues')
+plt.colorbar(label='Turnover Time (years)')
 
-# add labels to each subplot
-for i, ax in enumerate(axs):
-    # uppercase letters
-    ax.text(0, 1.05, chr(65 + i), transform=ax.transAxes, fontsize=14, va='top', ha='right')
+# Mark the calibrated points
+calibrated_colors = [colors[c] for c in ['dark_green', 'purple', 'red', 'dark_blue']]
+for my_model, color in zip(calibrated_models, calibrated_colors):
+    plt.scatter(my_model.t_max, my_model.t_min,
+                marker='o', color='none',
+                edgecolor=color, s=15, lw=1.5)
 
-# %% Save the figure
-out_fname = f'figures/fig2.png'
-plt.savefig(out_fname, dpi=600, bbox_inches='tight')
+plt.xlabel('$t_{max}$ (years)')
+plt.ylabel('$t_{min}$ (years)')
+plt.title('turnover time (T)')
+
+# Panel B -- contour plot of steady-state radiocarbon ratio
+plt.sca(axs['C'])
+plt.contourf(t_max_grid, t_min_grid, R_14C_matrix,
+             levels=10, cmap='Greys')
+plt.colorbar(label='$^{14}C / ^{12}C$')
+
+# Mark the calibrated points
+for my_model, color in zip(calibrated_models, calibrated_colors):
+    plt.scatter(my_model.t_max, my_model.t_min,
+                marker='o', color='none',
+                edgecolor=color, s=15, lw=1.5)
+plt.xlabel('$t_{max}$ (years)')
+plt.ylabel('$t_{min}$ (years)')
+plt.title(r'steady-state $^{14}C / ^{12}C$ ratio')
+
+# Plot the survival functions for the calibrated points
+plt.sca(axs['E'])
+plt.xscale('log')
+ages = np.logspace(-3, np.log10(1800), 200)
+for i, (my_model, color) in enumerate(
+    zip(calibrated_models, calibrated_colors)):
+    survival_fn = my_model.s(ages)
+    T_val = calibrated_Ts[i]
+    R_14C_val = calibrated_R_14Cs[i]
+    label=f'$T={int(T_val)}$; $^{{14}}C/^{{12}}C={R_14C_val:.2f}$'
+    plt.plot(ages, survival_fn, color=color, lw=2, label=label)
+plt.xlim(1e-2, 1800)
+plt.xlabel(r'age $\tau$ (years)')
+plt.ylabel(r'$s(\tau)$')
+plt.title('calibrated survival functions')
+# hiding the legend, will make in inkscape
+plt.legend(loc='upper right', fontsize=5,
+           frameon=False).set_visible(False)
+
+plt.sca(axs['F'])
+plt.xscale('log')
+# plot the CDF of the age distributions for the calibrated points
+for idx, (my_model, color) in enumerate(zip(calibrated_models, calibrated_colors)):
+    # ages = np.arange(2000)
+    ages = np.logspace(-3, np.log10(100000), 200)
+    age_dist_cdf = my_model.cdfA(ages)
+    plt.plot(ages, age_dist_cdf, color=color, lw=2)
+
+# Diagram the process of model validation from a single point estimate
+# of the CDF at age = 100 for example. 
+obs_age, obs_CDF = 10, 0.21
+plt.scatter([obs_age], [obs_CDF], color='none', 
+            edgecolor='k', marker='o', s=15, lw=1,
+            zorder=10)
+# draw a dashed line to the x-axis from this point
+plt.axvline(x=obs_age, ymin=0, ymax=obs_CDF,
+            color='grey', linestyle='--', lw=1, zorder=-1)
+ax = axs['F']
+ax.hlines(y=obs_CDF, xmin=ax.get_xlim()[0], xmax=obs_age,
+          colors='grey', linestyles='--', linewidth=1, zorder=-1)
+
+plt.xlim(1e-1, 100000)
+plt.xlabel(r'age $\tau$ (years)')
+plt.ylabel(r'CDF of $p_A(\tau)$')
+plt.title('calibrated age distributions')
+
+# Turn off axes for panels A and D
+axs['A'].axis('off')
+axs['D'].axis('off')
+
+plt.savefig('figures/fig2.png', dpi=300, bbox_inches='tight')
---
+++ notebooks/fig2_calcs.py
@@ -0,0 +1,54 @@
+# %% 
+# if currently in notebooks/ directory, go up one level
+import os
+import sys
+
+# Add the notebooks/ directory to the sys.path
+print(os.getcwd())
+
+# If we are in the notebooks/ directory, go up one level
+if os.path.basename(os.getcwd()) == 'notebooks':
+    sys.path.append(os.getcwd())
+    os.chdir('..')
+    print(f'Changed working directory to {os.getcwd()}')
+
+# %% imports
+import numpy as np
+
+from models import PowerLawDisKin
+from tqdm import tqdm
+
+# %% calculations needed to make figure 2
+# first scan over t_min and t_max to calculate T and R_14C
+# TODO: move all below here a new fig2.py once the 
+# simulation is importable. 
+# TODO: run this separately from the figure 2 plotting code
+# and save the results to a file, since it takes a while to run.
+
+# Exemplify calibration of the power-law model.
+# vary t_min and t_max and calculate the turnover T and
+# steady-state radiocarbon ratio. Store these in matrices
+# and plot as contour plots.
+t_min_values = np.linspace(0.1, 10, 100)
+t_max_values = np.linspace(1000, 100000, 100)
+t_min_grid, t_max_grid = np.meshgrid(
+    t_min_values, t_max_values, indexing='ij')
+
+T_matrix = np.zeros((len(t_min_values), len(t_max_values)))
+R_14C_matrix = np.zeros((len(t_min_values), len(t_max_values)))
+
+for i, t_min in enumerate(tqdm(t_min_values, desc='t_min loop')):
+    for j, t_max in enumerate(tqdm(t_max_values, desc='t_max loop', leave=False)):
+        my_model = PowerLawDisKin(t_min=t_min, t_max=t_max)
+        T_matrix[i, j] = my_model.T
+        R_14_C, _err = my_model.calc_radiocarbon_ratio_ss()
+
+        R_14C_matrix[i, j] = R_14_C
+
+# %% Save t_min, t_max, T_matrix, R_14C_matrix to a npz file
+np.savez('results/fig2_calcs.npz',
+         t_min_values=t_min_values,
+         t_max_values=t_max_values,
+         T_matrix=T_matrix,
+         R_14C_matrix=R_14C_matrix)
+
