f5a5c32e0e164b765f2bc13ce9bb8ae0d73ddc61
Avi Flamholz <flamholz@gmail.com>
2025-11-26 10:11:41 -0500

    print statistics pre/post filtering

--- notebooks/01_preprocess_balesdent_data.py
+++ notebooks/01_preprocess_balesdent_data.py
@@ -17,5 +17,21 @@ if __name__ == "__main__":
     raw_data_path = os.path.join(folder, raw_data_filename)
     raw_data = pd.read_excel(raw_data_path, skiprows=7)
 
+    # Count the number of unique locations in the raw data
+    print("Loaded raw data...")
+    unique_locations = raw_data[['Latitude', 'Longitude']].drop_duplicates()
+    print(f"\t{len(unique_locations)} unique locations in raw data.")
+    # Count the number of unique locations + duration labeling combinations
+    unique_loc_duration = raw_data[['Latitude', 'Longitude', 'Duration_labeling']].drop_duplicates()
+    print(f"\t{len(unique_loc_duration)} unique locations + durations in the raw data.")
+
+    print("Processing Balesdent et al. 2018 data...")
     final_data = process_balesdent_data(raw_data)
     final_data.to_csv(os.path.join('results/processed_balesdent_2018.csv'), index=False)
+
+    # Count the number of unique locations in the processed data
+    unique_locations_processed = final_data[['Latitude', 'Longitude']].drop_duplicates()
+    print(f"\t{len(unique_locations_processed)} unique locations in processed data.")
+    # Count the number of unique locations + duration labeling combinations
+    unique_loc_duration_processed = final_data[['Latitude', 'Longitude', 'Duration_labeling']].drop_duplicates()
+    print(f"\t{len(unique_loc_duration_processed)} unique locations + durations in the processed data.")
