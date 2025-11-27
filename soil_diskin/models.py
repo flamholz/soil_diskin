a4b318c3cc46e6c9624951a4a4dbd6aa462d6562
Avi Flamholz <flamholz@gmail.com>
2025-11-27 13:25:00 -0500

    add a base class for the diskin models. reduces redundancy, meaning the tests cover the codebase better

--- soil_diskin/models.py
+++ soil_diskin/models.py
@@ -104,7 +104,175 @@ class GlobalData:
         return LocDependentData(w=w, t=t, o=o, n=n, sand=sand, I=I, X0=X0)
 
 
-class GammaDisKin:
+class AbstractDiskinModel:
+    """Abstract base class defining the interface for DisKin models.
+
+    Note that the name DisKin refers to "disordered kinetics" models
+    that desribe decay rate distributions. The power law models are
+    somewhat distinct, as they describe variation with age, rather 
+    than static disorder as in the lognormal and gamma models. We have 
+    kept the name nonetheless. 
+    
+    It is expected that concrete subclasses expose analytically-calculated
+    values of the mean age and transit time at steady-state as properties
+    A and T.
+    """
+
+    def __init__(self, interp_r_14c=None):
+        """Initialize the model."""
+        self.interp_14c = interp_r_14c
+        if interp_r_14c is None:
+            self.interp_14c = INTERP_R_14C
+    
+        # These should be calculated by subclasses
+        self.T = None  # mean transit time at steady-state
+        self.A = None  # mean age at steady-state
+
+    def params_valid(self):
+        """Returns True if the parameters are valid, False otherwise."""
+        raise NotImplementedError("Subclasses must implement this method.")
+
+    def s(self, t):
+        """The survival function at age t.
+        
+        The survival function gives the fraction of input remaining at age t.
+        
+        Args:
+            t: float
+                The age at which to evaluate the survival function.
+        
+        Returns:
+            float
+                The value of the survival function at age t.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
+    
+    def pA(self, t):
+        """Calculate the probability density function of the age distribution.
+        
+        Default implementation uses pA(t) = s(t) / T, where s(t) is the 
+        survival function and T is the mean transit time at steady-state.
+
+        Reminder that pA(t) is a probability density function. Extracting 
+        a probability requires integrating pA(t) over an interval dt.
+
+        Args:
+            t: float
+                The age at which to evaluate the PDF.
+        
+        Returns:
+            float
+                The value of the PDF at age t.
+        """
+        return self.s(t) / self.T
+    
+    def cdfA(self, t):
+        """Calculate the cumulative distribution function of the age distribution.
+
+        The CDF is the integral of the PDF from 0 to t.
+        
+        Args:
+            t: float
+                The age at which to evaluate the CDF.
+        
+        Returns:
+            float
+                The value of the CDF at age t.
+        """
+        raise NotImplementedError("Subclasses must implement this method.")
+    
+    def mean_age_integrand(self, a):
+        """The integrand for calculating the mean age numerically.
+        
+        Args:
+            a: float
+                The age at which to evaluate the integrand.
+        
+        Returns:
+            float
+                The value of the integrand at age a.
+        """
+        return a*self.pA(a)
+    
+    def transit_time_integrand(self, a):
+        """The integrand for calculating the transit time numerically.
+        
+        Args:
+            a: float
+                The age at which to evaluate the integrand.
+        
+        Returns:
+            float
+                The value of the integrand at age a.
+        """
+        return self.s(a)
+    
+    def calc_mean_age(self, quad_limit=1500, quad_epsabs=1e-3):
+        """Calculate the mean age of the reservoir at steady-state 
+        by integrating the age distribution pA(t).
+
+        Args:
+            quad_limit: int
+                The maximum number of subintervals for the quadrature.
+            quad_epsabs: float
+                The absolute error tolerance for the quadrature.
+        
+        Returns:
+            A two-tuple of the mean age and an estimate of the absolute error.
+        """
+        return quad(self.mean_age_integrand, 0, np.inf,
+                    limit=quad_limit, epsabs=quad_epsabs)
+    
+    def calc_transit_time(self, quad_limit=1500, quad_epsabs=1e-3):
+        """Calculate the mean transit time T by numerical integration. 
+        
+        The steady-state transit time (turnover time) is given by 
+
+            T = \int_0^\infty t * pT(t) dt.
+        
+        where pT(t) is the transit time distribution. It can be shown that
+         
+           T = \int_0^\infty s(t) dt.
+
+        We use the latter relationship here
+
+        Returns:
+            A two-tuple of the mean transit time and an estimate of the absolute error.
+        """
+        return quad(self.transit_time_integrand, 0, np.inf,
+                    limit=quad_limit, epsabs=quad_epsabs)
+    
+    def radiocarbon_age_integrand(self, a):
+        """The integrand for calculating the radiocarbon age numerically.
+        
+        Default implementation uses pA from the subclass and the 
+        interpolated radiocarbon concentration provided at initialization.
+
+        Args:
+            a: float
+                The age at which to evaluate the integrand.
+        
+        Returns:
+            float
+                The value of the integrand at age a.
+        """
+        # Interpolation was done with x as years before present,
+        # so a is the correct input here
+        initial_r = self.interp_14c(a) 
+        radiocarbon_decay = np.exp(-LAMBDA_14C*a)
+        return initial_r * self.pA(a) * radiocarbon_decay
+
+    def calc_radiocarbon_ratio_ss(self, quad_limit=1500, quad_epsabs=1e-3):
+        """Calculate the radiocarbon age by integrating the age distribution.
+        
+        Returns:
+            A two-tuple of the radiocarbon age and an estimate of the absolute error.
+        """
+        return quad(self.radiocarbon_age_integrand, 0, np.inf,
+                    limit=quad_limit, epsabs=quad_epsabs)
+
+
+class GammaDisKin(AbstractDiskinModel):
     """A model where the rate distribution is gamma."""
     def __init__(self, a, b, interp_r_14c=None, I=None):
         """
@@ -121,43 +289,22 @@ class GammaDisKin:
         I: np.ndarray, optional
             An array of inputs to the model, representing the carbon input to the system.
         """
-        if a <= 0:
-            raise ValueError("Shape parameter a must be positive.")
-        if b <= 0:
-            raise ValueError("Scale parameter b must be positive.")
+        super().__init__(interp_r_14c=interp_r_14c)
 
         self.a = a  # shape parameter
         self.b = b  # scale parameter -- should not be zero
         self.I = I
-        self.interp_14c = interp_r_14c
-        if interp_r_14c is None:
-            self.interp_14c = INTERP_R_14C
 
         self.T = 1 / ((-1 + a) * b)
+        self.A = 1/((-2 + a) * (-1 + a) * b**2) / self.T
 
-    def radiocarbon_age_integrand(self, a):
-        # Interpolation was done with x as years before present,
-        # so a is the correct input here
-        initial_r = self.interp_14c(a) 
-        radiocarbon_decay = np.exp(-LAMBDA_14C * a)
-
-        return initial_r * self.pA(a) * radiocarbon_decay
-
-    def calc_radiocarbon_ratio_ss(self, quad_limit=1500, quad_epsabs=1e-3):
-        """Calculate the radiocarbon age by integrating the age distribution."""
-        return quad(self.radiocarbon_age_integrand, 0, np.inf,
-                    limit=quad_limit, epsabs=quad_epsabs)
+    def params_valid(self):
+        """Returns True if the parameters are valid, False otherwise."""
+        return self.a > 0 and self.b > 0
 
     def s(self, t):
         """the term for the amount of carbon in the system at age t"""
         return (1 + self.b * t) ** (-self.a)
-
-    def pA(self, a):
-        """Calculate the probability density function of the age distribution."""
-        return self.s(a) / self.T
-
-    def mean_age(self):
-        return 1/((-2 + self.a) * (-1 + self.a) * self.b**2) / self.T
 
     def cdfA(self, t):
         """Calculate the cumulative distribution function of the age distribution."""
@@ -166,7 +313,7 @@ class GammaDisKin:
         return cdf
 
 
-class GeneralPowerLawDisKin:
+class GeneralPowerLawDisKin(AbstractDiskinModel):
     """A model where rates of decay are proportional to 1/t between two bounding timescales.
 
     We call these bounding timescales tau_0 and tau_inf as in the notes. 
@@ -185,11 +332,10 @@ class GeneralPowerLawDisKin:
         I: np.ndarray, optional
             An array of inputs to the model, representing the carbon input to the system.
         """
+        super().__init__(interp_r_14c=interp_r_14c)
+
         if beta <= 0 or beta > 1:
-            raise ValueError("Beta must be between 0 and 1.")
-        
-        # if t_min >= t_max:
-        #     raise ValueError("t_min must be less than t_max.")
+            raise ValueError("Beta parameter must be in the range (0, 1].")
 
         self.t_min = t_min  # short time scale
         self.t_max = t_max  # long time scale
@@ -197,10 +343,6 @@ class GeneralPowerLawDisKin:
         self.beta = beta
 
         self.En = lambda n, x: x ** (n-1) * gammaincc(1 - n, x) * gamma(1 - n) if n < 1 else exp1(x)
-        
-        self.interp_14c = interp_r_14c
-        if interp_r_14c is None:
-            self.interp_14c = INTERP_R_14C
 
         # steady-state transit time
         tratio = (t_min * beta) / t_max
@@ -217,48 +359,30 @@ class GeneralPowerLawDisKin:
         self.product = prod  # vague name... 
         self.tratio = tratio
 
-
-    def radiocarbon_age_integrand(self, a):
-        # Interpolation was done with x as years before present,
-        # so a is the correct input here
-        initial_r = self.interp_14c(a) 
-        radiocarbon_decay = np.exp(-LAMBDA_14C*a)
-
-        return initial_r * self.pA(a) * radiocarbon_decay
-    
-    def calc_radiocarbon_ratio_ss(self, quad_limit=1500, quad_epsabs=1e-3):
-        """Calculate the radiocarbon age by integrating the age distribution."""
-        return quad(self.radiocarbon_age_integrand, 0, np.inf,
-                    limit=quad_limit, epsabs=quad_epsabs)
-
-    def mean_transit_time_integrand(self, a):
-        return self.s(a)
-    
-    def calc_mean_transit_time(self, quad_limit=1500, quad_epsabs=1e-3):
-        """Calculate the mean by integrating the transit time distribution."""
-        return quad(self.mean_transit_time_integrand, 0, np.inf,
-                    limit=quad_limit, epsabs=quad_epsabs)
+    def params_valid(self):
+        """Returns True if the parameters are valid, False otherwise."""
+        t_min_valid = self.t_min > 0
+        t_max_valid = self.t_max > 0
+        t_hierarchy_valid = self.t_max > self.t_min
+        beta_valid = (0 < self.beta <= 1)
+        return t_min_valid and t_max_valid and t_hierarchy_valid and beta_valid
 
     def s(self, t):
-        """the term for the amount of carbon in the system at age t"""
+        """The survival function at age t.
+        
+        The survival function gives the fraction of input remaining at age t.
+
+        Args:
+            t: float
+                The age at which to evaluate the survival function.
+        
+        Returns:
+            float
+                The fraction of input remaining at age t.
+        """
         num = self.product ** self.beta * np.exp(- t / self.t_max)
         denom = (self.product + t) ** self.beta
         return num / denom
-
-    def pA(self, a):
-        """Calculate the probability density function of the age distribution."""
-        return self.s(a) / self.T
-
-    def mean_age_integrand(self, a):
-        return a*self.pA(a)
-    
-    def calc_mean_age(self):
-        """Calculate the mean by integrating the age distribution.
-        
-        Returns:
-            A two-tuple of the mean age and an estimate of the absolute error.
-        """
-        return quad(self.mean_age_integrand, 0, np.inf)
     
     def impulse(self, t, X):
         """Calculate the change in state of the system at time t.
@@ -285,11 +409,13 @@ class GeneralPowerLawDisKin:
         cdf = 1 - (self.product / (self.product + a)) ** (self.beta - 1) * self.En(self.beta, (self.product + a) / self.t_max) / self.En(self.beta, self.tratio)
         return cdf
 
-class PowerLawDisKin:
+
+class PowerLawDisKin(AbstractDiskinModel):
     """A model where rates of decay are proportional to 1/t between two bounding timescales.
 
     We call these bounding timescales t_min and t_max.
     """
+
     def __init__(self, t_min, t_max, interp_r_14c=None, I=None):
         """
         Args:
@@ -304,6 +430,7 @@ class PowerLawDisKin:
         I: np.ndarray, optional
             An array of inputs to the model, representing the carbon input to the system.
         """
+        super().__init__(interp_r_14c=interp_r_14c)
         self.t_min = t_min  # short time scale
         self.t_max = t_max  # long time scale
         
@@ -321,6 +448,13 @@ class PowerLawDisKin:
 
         # mean age at steady-state
         self.A = (t_max * np.exp(-tratio)/e1_term) - t_min
+
+    def params_valid(self):
+        """Returns True if the parameters are valid, False otherwise."""
+        t_min_valid = self.t_min > 0
+        t_max_valid = self.t_max > 0
+        t_hierarchy_valid = self.t_max > self.t_min
+        return t_min_valid and t_max_valid and t_hierarchy_valid
 
     def s(self, tau):
         """The survival function at age tau."""
@@ -375,36 +509,15 @@ class PowerLawDisKin:
             np.power((self.e1_term * (self.t_min + tau)), -1) *
             np.exp(-(self.t_min + tau)/self.t_max))
         return initial_r * age_dist_term * radiocarbon_decay
-    
-    def calc_radiocarbon_ratio_ss(self, quad_limit=1500, quad_epsabs=1e-3):
-        """Calculate the steady-state radiocarbon ratio by integrating the age distribution."""
-        return quad(
-            self.radiocarbon_age_integrand, 0, np.inf,
-                limit=quad_limit, epsabs=quad_epsabs)
 
     def mean_transit_time_integrand(self, a):
         return self.t_min * np.exp(-a/self.t_max) / (self.t_min + a)
-    
-    def calc_mean_transit_time(self):
-        """Calculate the mean by integrating the transit time distribution."""
-        return quad(self.mean_transit_time_integrand, 0, np.inf)
     
     def pA(self, a):
         t0 = self.t_min
         tinf = self.t_max
         e1_term = self.e1_term
         return np.exp(-(t0 + a)/tinf) / ((t0 + a)*e1_term)
-
-    def mean_age_integrand(self, a):
-        return a*self.pA(a)
-    
-    def calc_mean_age(self):
-        """Calculate the mean by integrating the age distribution.
-        
-        Returns:
-            A two-tuple of the mean age and an estimate of the absolute error.
-        """
-        return quad(self.mean_age_integrand, 0, np.inf)
     
     def impulse(self, t, X):
         """Calculate the change in state of the system at time t.
@@ -431,7 +544,9 @@ class PowerLawDisKin:
         tratio = self.t_min / self.t_max
         return (1 - exp1((self.t_min + a) / self.t_max) / exp1(tratio))
 
-class LognormalDisKin:
+
+class LognormalDisKin(AbstractDiskinModel):
+    """A model where the rate distribution is lognormal."""
 
     def __init__(self, mu, sigma, k_min=None, k_max=None, interp_r_14c=None, N=1000, ppf_lim=1e-5):
         """
@@ -453,9 +568,8 @@ class LognormalDisKin:
         ppf_lim: float
             The percent point function limit for the lognormal distribution.
         """
-        if sigma < 0:
-            raise ValueError("Sigma must be non-negative.")
-        
+        super().__init__(interp_r_14c=interp_r_14c)
+
         self.mu = mu
         self.k_star = np.exp(mu)
         self.sigma = sigma
@@ -470,14 +584,11 @@ class LognormalDisKin:
         self.q_min = np.log(self.kappa_min)
         self.q_max = np.log(self.kappa_max)        
 
-        self.interp_14c = interp_r_14c
-        if interp_r_14c is None:
-            self.interp_14c = INTERP_R_14C
-
-        # steady-state transit time
+        # steady-state transit time and mean age
         self.T = np.exp(-self.mu + ((self.sigma**2) / 2))
         # mean age at steady-state
-        self.a = self.T * np.exp(self.sigma**2)
+        self.A = self.T * np.exp(self.sigma**2)
+        
         self.ks = np.logspace(self.q_min, self.q_max, N, base=np.e)
         self.I = lognorm.pdf(self.ks, s=self.sigma, scale=np.exp(self.mu))
 
@@ -504,16 +615,62 @@ class LognormalDisKin:
         sigma_squared = np.log(a/T)
         sigma = np.sqrt(sigma_squared)
         mu = sigma_squared/2 - np.log(T)
-
-        print(mu, sigma)
         return cls(mu, sigma, N=N)
-
-    def _pA_alpha_logscale_integrand(self, alpha, q):
-        """Integrand for the age distribution in log scale."""
-        p_q = norm.pdf(q, loc=0, scale=self.sigma)
-        exp_factor = np.exp(-alpha*np.exp(q))
-        return p_q*exp_factor
     
+    def params_valid(self):
+        """Returns True if the parameters are valid, False otherwise."""
+        return self.sigma > 0
+    
+    def _pk(self, k):
+        """The probability density function of the rate distribution p(k)."""
+        return lognorm.pdf(k, s=self.sigma, scale=np.exp(self.mu))
+    
+    def _s_integrand(self, t, k):
+        """Integrand for the survival function."""
+        return self._pk(k) * np.exp(-k * t)
+
+    def s(self, t):
+        """The survival function at age t.
+        
+        The survival function gives the fraction of input remaining at age t.
+
+        For the lognormal model, the survival function is given by
+
+            𝑠(𝑡)= ∫_0^\infty (p(k) exp(-kt) dk)
+
+        where p(k) is a lognormal distribution over ks. Since this 
+        is the Laplace transform of a lognormal distribution, there is no
+        known closed-form solution, so we evaluate the integral numerically.
+
+        This integral is not very stable in general, and especially using 
+        scipy methods. In practice we resort to separate calculation in 
+        Mathematica. 
+
+        Args:
+            t: float
+                The age at which to evaluate the survival function.
+        
+        Returns:
+            float
+                The fraction of input remaining at age t.
+        """
+        # We picked some limits of integration based on the p(k)
+        # distribution in the constructor.
+        k_min = self.k_min
+        k_max = self.k_max
+        result, _ = quad(
+            self._s_integrand, k_min, k_max, args=(t,),
+            limit=500, epsabs=1e-5)
+        return result
+    
+    def cdfA(self, t):
+        """Calculate the cumulative distribution function of the age distribution."""
+        # The CDF is the integral of the PDF from 0 to a
+        result, _ = quad(
+            self.pA, 0, t,
+            limit=500, epsabs=1e-5)
+        return result
+        
     def _dX(self, t, X):
         """Calculate the change in state of the system at time t.
 
